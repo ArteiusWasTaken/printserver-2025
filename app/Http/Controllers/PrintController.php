66e15c332e246072b79fccc7aed3d21c0ed491d7
Alexander <balexander.rosas@gmail.com>
2025-06-24 17:13:05 -0600

    new tests 7

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -549,6 +549,8 @@ class PrintController extends Controller
             if ($extension !== 'zpl' && $marketplace->marketplace !== 'MERCADOLIBRE') {
                 $pythonScript = $extension === 'pdf' ? 'pdf_to_thermal.py' : 'image_to_thermal.py';
                 $output = trim(shell_exec("python3 python/label/convert/{$pythonScript} '{$nombreArchivo}' '{$documento->zoom_guia}' 2>&1"));
+                $pythonScript = $extension === 'pdf' ? 'pdf_to_zpl.py' : 'image_to_zpl.py';
+                $output = trim(shell_exec("python3 python/afa/{$pythonScript} '{$output}' 2>&1"));
                 $archivoFinal = $output;
             } else {
                 $archivoFinal = $nombreArchivo;
