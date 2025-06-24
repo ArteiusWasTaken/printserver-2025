9e38b52fc122c195c647b563ba7ecdfce605ebec
Alexander <balexander.rosas@gmail.com>
2025-06-24 16:02:01 -0600

    Revert "gtest print 4"
    This reverts commit cdc1261f

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -500,6 +500,7 @@ class PrintController extends Controller
                     ]);
                 }
 
+                // Ahora sí conviertes a base64
                 $archivosImpresion[] = base64_encode($content);
 
                 if (str_ends_with($archivo->nombre, '.zpl')) {
@@ -547,52 +548,27 @@ class PrintController extends Controller
             chmod($nombreArchivo, 0777);
 
             if ($extension !== 'zpl' && $marketplace->marketplace !== 'MERCADOLIBRE') {
-                $pythonScript = $extension === 'pdf' ? 'pdf_to_zpl.py' : 'image_to_zpl.py';
-                $zpl = shell_exec("python3 python/afa/{$pythonScript} '{$nombreArchivo}' 2>&1");
-
-                if (!$zpl) {
-                    return response()->json([
-                        'code' => 500,
-                        'message' => 'Error al convertir a ZPL con script Python',
-                    ]);
-                }
-                $contenidoAEnviar = $zpl;
+                $pythonScript = $extension === 'pdf' ? 'pdf_to_thermal.py' : 'image_to_thermal.py';
+                $output = trim(shell_exec("python3 python/label/convert/{$pythonScript} '{$nombreArchivo}' '{$documento->zoom_guia}' 2>&1"));
+                $archivoFinal = $output;
             } else {
-                $contenidoAEnviar = file_get_contents($nombreArchivo);
+                $archivoFinal = $nombreArchivo;
             }
 
-            try {
-                $socket = fsockopen($ipImpresora, 9100, $errno, $errstr, 5);
-                if (!$socket) {
-                    throw new Exception("No se pudo conectar a la impresora: $errstr ($errno)");
-                }
+            $modo = ($extension === 'zpl' || $marketplace->marketplace === 'MERCADOLIBRE') ? '-o raw' : '';
+            exec("lp -d {$ipImpresora} -n 1 {$modo} {$archivoFinal}");
+            $S = "lp -d {$ipImpresora} -n 1 {$modo} {$archivoFinal}";
+            $outputs[] = $archivoFinal;
 
-                fwrite($socket, $contenidoAEnviar);
-                fclose($socket);
-            }
-            catch (Exception $e) {
-                ErrorLoggerService::logger(
-                    'Error en etiquetas. Impresora: ' . $ipImpresora,
-                    'PrintController',
-                    [
-                        'exception' => $e->getMessage(),
-                        'line' => self::logLocation()
-                    ]
-                );
-                return response()->json([
-                    'Error' => 'No se pudo imprimir: ' . $e->getMessage()
-                ], 500);
-            }
-
-            $outputs[] = $nombreArchivo;
-
+            if (file_exists($archivoFinal)) unlink($archivoFinal);
             if (file_exists($nombreArchivo)) unlink($nombreArchivo);
         }
 
         return response()->json([
             'code' => 200,
-            'message' => 'Guías enviadas a impresión. '. $ipImpresora,
+            'message' => 'Guías enviadas a impresión.',
             'outputs' => $outputs,
+            'a' => $S ??[]
         ]);
     }
 
