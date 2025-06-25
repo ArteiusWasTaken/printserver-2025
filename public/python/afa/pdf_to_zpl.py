134ba0a043e200654579ebfe4c6e1b7044717a8e
Alexander <balexander.rosas@gmail.com>
2025-06-25 11:47:43 -0600

    new tests 30

--- public/python/afa/pdf_to_zpl.py
+++ public/python/afa/pdf_to_zpl.py
@@ -41,5 +41,5 @@ if __name__ == "__main__":
         sys.exit(f"Archivo no encontrado: {pdf_file}")
 
     zpl_code = convert_pdf_to_zpl(pdf_file, label_width=812, label_height=1624, invert=True)
-    print(zpl_code.decode('utf-8').replace('\n', ''))
+    print(zpl_code.decode('utf-8').replace('\n', ' '))
 
