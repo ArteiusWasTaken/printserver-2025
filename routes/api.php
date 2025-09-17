f5a64806af2b1f896f51487980534a1683c55655
Alexander Rosas <sebastiancifer@gmail.com>
2025-09-17 09:25:46 -0700

    Try 1

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'dev'], function () {
     Route::get('usb/{barcode}', [PrintController::class, 'ticketsUsb']);
-    Route::get('me', phpinfo());
 });
 
 Route::group(['middleware' => [JwtMiddleware::class]], function () {
